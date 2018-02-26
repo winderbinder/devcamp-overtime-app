b5ea5c6016be8f3949d36489eb88708b696aa42f
winderbinder <junior_19000@hotmail.com>
2018-02-26 15:31:01 -0700

    added tests for admin login

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,15 +1,23 @@
 # All Administrate controllers inherit from this `Admin::ApplicationController`,
 # making it the ideal place to put authentication logic or other
-# before_actions.
+# before_filters.
 #
 # If you want to add pagination or other controller-level concerns,
 # you're free to overwrite the RESTful controller actions.
 module Admin
+  def self.admin_types
+    ['AdminUser']
+  end
+
   class ApplicationController < Administrate::ApplicationController
+    before_action :authenticate_user!
     before_action :authenticate_admin
 
-    def authenticate_admin
-      # TODO Add authentication logic here.
+    def authenticate_admin 
+      unless Admin.admin_types.include?(current_user.try(:type))
+        flash[:alert] = "PERMISSION DENIED."
+        redirect_to(root_path)
+      end
     end
 
     # Override this value to specify the number of elements to display at a time
