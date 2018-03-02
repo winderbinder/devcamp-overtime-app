73d7c4176f21fa60c7e8de182940eca15feaebef
winderbinder <junior_19000@hotmail.com>
2018-03-02 14:04:22 -0700

    implemented labels for publish status

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,18 @@
 module PostsHelper
+  def status_label status
+    status_span_generator status
+  end
+
+  private
+
+  def status_span_generator status
+  case status
+    when 'submitted'
+      content_tag(:span, status, class: 'label label-primary') # content tag will generate stuff
+    when 'approved'
+      content_tag(:span, status, class: 'label label-success')
+    when 'rejected'
+      content_tag(:span, status, class: 'label label-danger')
+    end
+  end
 end
