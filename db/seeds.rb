79a456b19c4fae1f26f3be375222b19fa05ed89e
winderbinder <junior_19000@hotmail.com>
2018-03-06 15:46:22 -0700

    updated seeds file for audit logs

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,7 +29,7 @@ end
 puts "100 user posts created"
 
 100.times do |audit_log|
-  AuditLog.create!(user_id: User.last.id, status: 0, start_date: (Date.today - 6.days))
+  AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
 
 end
 
