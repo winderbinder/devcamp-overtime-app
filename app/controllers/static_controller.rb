147bd48a2a9f5dbfa2b528d655ca00678b2fea7d
winderbinder <junior_19000@hotmail.com>
2018-03-07 14:00:10 -0700

    setup the prototype employee homepage

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,6 +1,10 @@
 class StaticController < ApplicationController
   def homepage
-    @pending_approvals = Post.submitted?
+    if admin_types.include?(current_user.type)
+    @pending_approvals = Post.submitted
     @recent_audit_items = AuditLog.last(10)
+  else
+    #something else for later
+    end
   end
 end
